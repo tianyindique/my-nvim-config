fb13f9190dcda711311a8eb68da7ad43c282409f
tianyindique <tianyindique@outlook.com>
2025-09-09 21:49:09 +0800

    Add: Added a autocmd in options.lua to make the tab turn to 2 space

--- lua/options.lua
+++ lua/options.lua
@@ -2,11 +2,25 @@ vim.opt.clipboard = 'unnamedplus'  -- use system clipboard
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect'}
 vim.opt.mouse = 'a' -- allow the mouse to be used in Nvim
 
--- Tab
+-- Tab(global)
 vim.opt.tabstop = 4 --number of visual spaces per TAB
 vim.opt.softtabstop = 4 -- number of spacesin tab when editing
 vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
 vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+
+-- the tab in .c file
+local indent_group = vim.api.nvim_create_augroup("IndentSettings", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = indent_group,
+  pattern = "c",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
 
 -- UI config
 vim.opt.number = true -- show absolute number
