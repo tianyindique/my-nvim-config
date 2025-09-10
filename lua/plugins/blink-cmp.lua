42c5b3b0a83f479454ce16f40b7d80712f66b852
tianyindique <tianyindique@outlook.com>
2025-09-10 13:06:57 +0800

    Add: Added tiny-inline-diagnostic.nvim to show lsp disgnostic better

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -34,8 +34,13 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
-
+    completion = { 
+      documentation = { auto_show = false }, 
+      ghost_text = {
+        enabled = true,
+        hl_group = "Comment",
+      },
+    },
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
